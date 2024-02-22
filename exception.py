b8901702f2daf3d60932221fb34a35d2b71dfdf9
Nidhish Wakodikar <nidhishwakodikar@gmail.com>
2024-02-22 13:03:37 +0530

    logger and exception file completed

---
+++ exception.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from src.logger import logging
+from src.exception import CustomException
+import os,sys
+app = Flask(__name__)
+
+@app.route('/',methods = ['GET','POST'])
+
+def index():
+    try:
+        raise Exception("we are testing our Exception file")
+    except Exception as e:
+        ML= CustomException(e,sys)
+        logging.info(ML.error_message)
+    logging.info("we are testing logging file")
+
+    return " Welcome  to  Nidhish"
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ logs.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from src.logger import logging
+
+
+app = Flask(__name__)
+
+@app.route('/',methods = ['GET','POST'])
+
+def index():
+    logging.info("we are testing logging file")
+
+    return " Welcome  to  Nidhish"
+
+if __name__ == "__main__":
+    app.run(debug=True)
